a1eab8d230464dc048266b610b24852fbd57d45b
nf <nicholas.facchin@finconsgroup.com>
2021-05-27 13:59:19 +0200

    raggruppa per periodo

--- script.js
+++ script.js
@@ -12,44 +12,49 @@ fetch("vaccini-per-fascia/data/somministrazioni-vaccini-latest.json")
     const data = res.data;
 
     const regioni = groupBy(data, "nome_area");
-    const select = createSelectRegioni(regioni);
-    createChart(data, "Abruzzo");
-    select.addEventListener("change", (event) => {
-      console.log(event);
+    const selectRegioni = createSelectRegioni(regioni);
+    const periodo = document.getElementById("periodo");
+    createChart(JSON.parse(JSON.stringify(data)), "Abruzzo", "settimana");
+    periodo.addEventListener("change", (event) => {
+      Object.keys(charts).forEach((chart) => {
+        charts[chart].destroy();
+      });
+      const regione = selectRegioni.value;
+      createChart(
+        JSON.parse(JSON.stringify(data)),
+        regione,
+        event.target.value
+      );
+    });
+    selectRegioni.addEventListener("change", (event) => {
       Object.keys(charts).forEach((chart) => {
         charts[chart].destroy();
       });
 
-      createChart(data, event.target.value);
+      createChart(
+        JSON.parse(JSON.stringify(data)),
+        event.target.value,
+        periodo.value
+      );
     });
   })
   .catch((error) => console.error(error));
 
-function createChart(data, regione) {
-  const venetoData = data.filter((item) => item.nome_area === regione);
-  const fasce = groupBy(venetoData, "fascia_anagrafica");
+function createChart(data, regione, periodo) {
+  const dataByRegione = data.filter((item) => item.nome_area === regione);
+  const fasce = groupBy(dataByRegione, "fascia_anagrafica");
 
-  const fasceByFornitore = {};
-  Object.keys(fasce).forEach((fascia) => {
-    fasce[fascia].forEach((item) => {
-      if (!fasceByFornitore[fascia]) {
-        fasceByFornitore[fascia] = {};
-      }
-      if (!fasceByFornitore[fascia][item.fornitore]) {
-        fasceByFornitore[fascia][item.fornitore] = 0;
-      }
-      fasceByFornitore[fascia][item.fornitore] =
-        fasceByFornitore[fascia][item.fornitore] + item.prima_dose;
-    });
-  });
-  const colorScheme = ["#25CCF7", "#FD7272", "#54a0ff", "#00d2d3", "#1abc9c"];
+  const fasceByPeriodo = getFasceByPeriodo(periodo, fasce);
 
+  const fasceByFornitore = getFasceByFornitore(fasceByPeriodo);
+  const colorScheme = ["#25CCF7", "#FD7272", "#54a0ff", "#00d2d3"];
+  console.log(fasceByFornitore);
   Object.keys(fasceByFornitore).forEach((fascia) => {
     const chartData = {
       labels: [],
       datasets: [
         {
-          label: "pippo",
+          label: fascia,
           data: [],
           hoverOffset: 4,
           backgroundColor: colorScheme,
@@ -84,6 +89,68 @@ function createChart(data, regione) {
       },
     });
   });
+}
+
+function getFasceByFornitore(fasceByPeriodo) {
+  const fasceByFornitore = {};
+  Object.keys(fasceByPeriodo).forEach((fascia) => {
+    fasceByPeriodo[fascia].forEach((item) => {
+      if (!fasceByFornitore[fascia]) {
+        fasceByFornitore[fascia] = {
+          Janssen: 0,
+          Moderna: 0,
+          "Pfizer/BioNTech": 0,
+          "Vaxzevria (AstraZeneca)": 0,
+        };
+      }
+      fasceByFornitore[fascia][item.fornitore] =
+        fasceByFornitore[fascia][item.fornitore] + item.prima_dose;
+    });
+  });
+  return fasceByFornitore;
+}
+
+function getFasceByPeriodo(periodo, fasce) {
+  let fasceByPeriodo = {};
+  if (periodo !== "sempre") {
+    Object.keys(fasce).forEach((fascia) => {
+      fasce[fascia].forEach((item) => {
+        let previusRange;
+        switch (periodo) {
+          case "settimana":
+            previusRange = moment(new Date()).subtract(7, "d");
+            break;
+
+          case "1_mese":
+            previusRange = moment(new Date()).subtract(1, "month");
+            break;
+          case "3_mesi":
+            previusRange = moment(new Date()).subtract(3, "month");
+            break;
+          case "6_mesi":
+            previusRange = moment(new Date()).subtract(6, "month");
+            break;
+
+          default:
+            break;
+        }
+        if (!fasceByPeriodo[fascia]) {
+          fasceByPeriodo[fascia] = [];
+        }
+        if (
+          moment(item.data_somministrazione).isBetween(
+            previusRange.format(),
+            moment(new Date()).format()
+          )
+        ) {
+          fasceByPeriodo[fascia].push(item);
+        }
+      });
+    });
+  } else {
+    fasceByPeriodo = fasce;
+  }
+  return fasceByPeriodo;
 }
 
 function createSelectRegioni(regioni) {
